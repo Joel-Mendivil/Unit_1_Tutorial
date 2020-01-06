0a22976e7f6935c8c05a444974ba5d00212a7d84
Joel Mendivil <jmendivil171@west-mec.org>
2020-01-06 15:26:19 -0700

    Started on Tutorial

---
+++ tny_script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Joel Mendivil
+   Date: 1/6/19  
+
+*/
+
+MediaStreamAudioSourceNode();
+
+document.getElementById("dateNow");
+
+document.getSelection.name("Jeff");
